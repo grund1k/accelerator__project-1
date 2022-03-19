a56ca5f83cc8461b8409dceed8fac63af971b056
grund1k <dfyz8607@gmail.com>
2022-03-19 21:25:44 +0300

    Добавил оверлей

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -2,6 +2,19 @@ import {clearNoJsCode} from '../utils/clear-no-js';
 
 const header = document.querySelector('.header');
 const closeMenuButton = header.querySelector('.nav__modal-toggle');
+const overlay = document.querySelector('.overlay');
+
+const switchOverlay = (status) => {
+  if (overlay) {
+    if (status === 'hidden') {
+      overlay.classList.remove('overlay--active');
+    }
+
+    if (status === 'active') {
+      overlay.classList.add('overlay--active');
+    }
+  }
+};
 
 clearNoJsCode(header, 'header');
 header.classList.remove('header--nav-open');
@@ -11,6 +24,7 @@ const openMenu = () => {
   if (header) {
     header.classList.add('header--nav-open');
     header.classList.remove('header--nav-close');
+    switchOverlay('active');
 
     closeMenuButton.addEventListener('click', closeMenu);
   }
@@ -20,6 +34,7 @@ const closeMenu = () => {
   if (header) {
     header.classList.add('header--nav-close');
     header.classList.remove('header--nav-open');
+    switchOverlay('hidden');
 
     closeMenuButton.removeEventListener('click', closeMenu);
   }
